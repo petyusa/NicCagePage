256c220aabd5cc740c69ac3f4cad307a018cc827
gergogazd <gergogazd@gmail.com>
2017-05-15 05:05:21 +0200

    Add files via upload

--- indexjs.js
+++ indexjs.js
@@ -1,8 +1,40 @@
 var mainContainer = document.getElementById('mainContainer');
 
-load("cast");
-load("crew");
+refresh();
+
+function refresh(){
+	mainContainer.innerText="";
+	load("cast");
+	load("crew");
+}
+
+function search(film){ //találat lista szűrése
+	var filter = document.getElementById('txtSearch').value.toLowerCase();
 	
+	if (film.title) if (film.title.toLowerCase().indexOf(filter) !== -1) return true; //találat címben
+	
+	if (film.original_title) if (film.original_title.toLowerCase().indexOf(filter) !== -1) return true; //találat eredeti címben
+	
+	if (film.overview) if (film.overview.toLowerCase().indexOf(filter) !== -1) return true; //találat cselekményben
+	
+	if (film.release_date) if (film.release_date.substring(0, 4) == filter) return true; //évszám egyezik
+	
+	for (var i = 0; i< film.genres.length; ++i){
+		if (film.genres[i].name.toLowerCase().indexOf(filter) !== -1) return true; //találat műfajban
+	}
+	
+	for (var i = 0; i< film.production_companies.length; ++i){
+		if (film.production_companies[i].name.toLowerCase().indexOf(filter) !== -1) return true; //találat gyártók között
+	}
+	
+	for (var i = 0; i< film.credits.cast.length; ++i){
+		if (film.credits.cast[i].character.toLowerCase().indexOf(filter) !== -1) return true; //találat szereplők között
+		else if (film.credits.cast[i].name.toLowerCase().indexOf(filter) !== -1) return true; //találat színészek között
+	}
+	
+	return false; //nincs találat
+}
+
 function reszletek(id, role){
 	window.location.assign('movie.html?id='+id+'&role='+role);
 }
@@ -19,6 +51,7 @@ function load(role){
 			return;
 	}
 	for (var i = 0; i < filmek.length; i++) {
+		if (!search(filmek[i])) continue; //ha nem felel meg a szűrés feltételeinek, továbblép a következő filmre
 
 		var movieElem = document.createElement('div');
 		movieElem.classList.add("lenyiloElem", role, "hide"); 
@@ -34,7 +67,7 @@ function load(role){
 		movieElem.appendChild(bovebben);
 		bovebben.classList.add("bovebb");
 
-		//Műfajok kiszedése az ojjketumból
+		//Műfajok kiszedése az objektumból
 		var genreTomb =[];
 		var genreStr;
 		for (var j=0; j<filmek[i].genres.length; j++){
@@ -42,7 +75,7 @@ function load(role){
 		}
 		genreStr = genreTomb.join(', ');
 		
-		//Production companies kiszedése az ojjketumból
+		//Production companies kiszedése az objektumból
 		var companiesTomb =[];
 		var companiesStr;
 		for (var k=0; k<filmek[i].production_companies.length; k++){
@@ -50,7 +83,7 @@ function load(role){
 		}
 		companiesStr = companiesTomb.join(', ');
 		
-		//Nic cage karakterének kiszedése az ojjketumból
+		//Nic cage karakterének kiszedése az objektumból
 		var nicKaraktereTomb = [];
 		var nicKaraktereStr = '';
 		
@@ -112,12 +145,15 @@ function init() {
 }
 
 function toggleElem() {
+	init();
+	var show = false;
+	if (event.target.parentNode.classList.contains('hide')) show = true;
 	var elemClassNeve = event.target.parentNode.className;
 	// mindent hide-ra állít
 	for ( var i = 0; i < lenyiloElemek.length; i++ ) {
-		lenyiloElemek[i].classList.add('hide');// = 'lenyiloElem hide';
+		lenyiloElemek[i].classList.add('hide');
 	}
 	
-	// ha van hide-ja, vegye le
-	event.target.parentNode.classList.remove('hide');// = 'lenyiloElem';
+	// ha eddig is rajta volt a hide, vegye le
+	if (show) event.target.parentNode.classList.remove('hide');
 }
